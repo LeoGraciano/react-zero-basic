de4175c1365cd8fa9e57d907171dafebc306f3cc
Léo Graciano <leonardoferreiragraciano@gmail.com>
2023-12-27 21:00:02 -0300

    feat: add form login

---
+++ src/app/pages/login/Login.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.info(email, password);
+    navigate("/pagina-inicial");
+  };
+  return (
+    <div>
+      <form>
+        <label>
+          <span>Email</span>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          <span>Senha</span>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="button" onClick={handleLogin}>
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
+};
